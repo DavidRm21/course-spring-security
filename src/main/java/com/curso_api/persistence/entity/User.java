6afbc2f0422ec6833fe8687f67ac349c98f36197
DavidRm21 <cdavidromero214@gmail.com>
2024-08-07 20:04:02 -0500

    Creacion de metodo que captura los permisos segun el role

--- src/main/java/com/curso_api/persistence/entity/User.java
+++ src/main/java/com/curso_api/persistence/entity/User.java
@@ -6,17 +6,19 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "\"user\"")
 @Getter
 @Setter
 @Builder
-public class User implements UserDetails {
+public class User implements UserDetails { // UserDetails es el usuario logeado
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -24,11 +26,21 @@ public class User implements UserDetails {
     private String name;
     private String password;
     @Enumerated(EnumType.STRING)
-    private Role role;
+    private Role role; // Clase para la base de datos, más dinamica
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        if (this.role == null) return null;
+        if (this.role.getPermissions() == null) return null;
+
+        return role.getPermissions().stream()
+                .map(Enum::name)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+//            .map( each -> {
+//            String permission = each.name();
+//            return new SimpleGrantedAuthority(permission);
+//        }).collect(Collectors.toList());
     }
 
     @Override
